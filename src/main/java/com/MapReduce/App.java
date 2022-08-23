693d914ffbdc8157a6c229cf9580409ab3094df2
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2022-08-23 12:05:04 +0530

    Add logic for directory listing

--- src/main/java/com/MapReduce/App.java
+++ src/main/java/com/MapReduce/App.java
@@ -1,15 +1,22 @@
 package com.MapReduce;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Enter directory path: ");
-        var dataDir = new File("./");
-        var dirList = dataDir.list();
-
-        for (var dirItem : dirList) {
-            System.out.println(dirItem.toString());
+        var sc = new Scanner(System.in);
+        var dataPath = sc.nextLine();
+        var dataDir = new File(dataPath);
+        var dirList = dataDir.listFiles();
+        if (dirList != null) {
+            for (var dirItem : dirList) {
+                if (dirItem.isFile()) {
+                    System.out.println(dirItem.getName());
+                }
+            }
         }
+        sc.close();
     }
 }
